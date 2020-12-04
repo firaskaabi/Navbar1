f9acc3546198965376c2cc8cb78611a6cd5cf916
firaskaabi <36204186+firaskaabi@users.noreply.github.com>
2020-12-04 04:51:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+
+document.getElementById('home').addEventListener("click",()=>{
+  container.classList.toggle("active");
+})
+document.getElementById('service').addEventListener("click",()=>{
+  container.classList.toggle("active");
+})
+document.getElementById('profil').addEventListener("click",()=>{
+  container.classList.toggle("active");
+})
